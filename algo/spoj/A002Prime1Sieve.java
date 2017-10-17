bff9b0938750fe7f8f4eed67e5cdbb2f53272669
Sunaina Pai <sunainapai.in@gmail.com>
2017-10-17 23:03:17 +0530

    Spoj: Create sieve only once

--- algo/spoj/A002Prime1Sieve.java
+++ algo/spoj/A002Prime1Sieve.java
@@ -4,8 +4,6 @@
 // TLE
 
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class A002Prime1Sieve {
@@ -16,21 +14,29 @@ public class A002Prime1Sieve {
         int m;
         int n;
 
+        boolean[] primes = sieveOfEratosthenes((int)Math.pow(10, 9));
+
         for (int i = 0; i < nTests; i++) {
             m = sc.nextInt();
             n = sc.nextInt();
-            sieveOfEratosthenes(m, n);
+            for (int j = m; j <= n; j++) {
+                if (primes[j]) {
+                    System.out.println(j);
+                }
+            }
             System.out.println();
         }
     }
 
-    public static void sieveOfEratosthenes(int m, int n) {
+    public static boolean[] sieveOfEratosthenes(int n) {
         boolean[] primes = new boolean[n + 1];
         for (int i = 2; i < primes.length; i++) {
             primes[i] = true;
         }
 
-        for (int p = 2; p <= Math.round(Math.sqrt(n)); p++) {
+        int sqrtN = (int) (Math.sqrt(n)) + 1;
+
+        for (int p = 2; p <= sqrtN; p++) {
             if (primes[p] == true) {
                 // p is prime because if it was not it would have been
                 // marked as a multiple of some other smaller prime.
@@ -42,12 +48,7 @@ public class A002Prime1Sieve {
             }
         }
 
-        // Print all primes between m and n ignoring everything < m.
-        for (int i = m; i < primes.length; i++) {
-            if (primes[i]) {
-                System.out.println(i);
-            }
-        }
+        return primes;
     }
 
 }
@@ -55,8 +56,9 @@ public class A002Prime1Sieve {
 class A002Test {
 
     public static void main(String[] args) {
-        A002Prime1Sieve.sieveOfEratosthenes(1, 5);
-        // Output 2, 3, 5
+        boolean[] actual = A002Prime1Sieve.sieveOfEratosthenes(5);
+        boolean[] expected = {false, false, true, true, false, true};
+        System.out.println(Arrays.equals(actual, expected) ? "PASS" : "FAIL");
     }
 
 }
