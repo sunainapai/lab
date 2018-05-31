7ecc3264756503a78ab4fc0515df7abe52a4aae9
Sunaina Pai <sunainapai.in@gmail.com>
2018-05-31 22:11:14 +0530

    Practical Common Lisp: Invoke function and lambda in more ways

--- lisp/practical-common-lisp/19-hash-quote.lisp
+++ lisp/practical-common-lisp/19-hash-quote.lisp
@@ -5,6 +5,12 @@
 (f 10)
 (f #'f) ; Pass function
 (f 'f)  ; Pass function name
+(format t "~%")
+
+(f #'(lambda (x) x)) ; Pass lambda
+(f '(lambda (x) x))  ; Pass lambda name
+(f (lambda (x) x))   ; Also seems to pass lambda
+(format t "~%")
 
 ; (f f) ; The variable F is unbound.
 
@@ -12,12 +18,26 @@
 (defun invoker (fn arg1)
   (funcall fn arg1))
 
+; The value 10 is not of type (OR FUNCTION SYMBOL)
+; (invoker 10 "foo")
+
 ; Call the higher order function with a function and a string.
-(invoker #'f "baz")
+(invoker #'f "bar") ; funcall can invoke a function
+(invoker 'f "baz")  ; funcall can invoke a function by name too
+(format t "~%")
 
 ; Instead of a named function, pass a lambda (anonymous function) this time.
 (invoker #'(lambda (x)
-             (format t "lambda: x: ~a~%" x)) 20)
+             (format t "lambda: x: ~a~%" x)) 10)
+
+; The value (LAMBDA (X) (FORMAT T "lambda: x: ~a~%" X)) is not of type
+; (OR FUNCTION SYMBOL)
+; (invoker '(lambda (x)
+;             (format t "lambda: x: ~a~%" x)) 20)
+
+(invoker (lambda (x)
+           (format t "lambda: x: ~a~%" x)) 30)
+(format t "~%")
 
 (defun g (x)
   (format t "g: x: ~a~%" x)
@@ -34,8 +54,8 @@
                (format t "lambda: x + z: ~a~%" (+ x z))) 30))
 
 (g 40)
+(format t "~%")
 
 (h 100) ; h is available in this scope.
-
 (fmakunbound 'g)
 (h 200) ; h is still available even if g isn't anymore.
