cac230921107cf3b6d0baef6a47a28a7683b4cdb
Sunaina Pai <sunainapai.in@gmail.com>
2018-05-31 22:10:47 +0530

    Practical Common Lisp: setf with plist property

--- lisp/practical-common-lisp/16-setf.lisp
+++ lisp/practical-common-lisp/16-setf.lisp
@@ -1,3 +1,10 @@
 (defvar *a* nil)
 (setf *a* "hello, world~%")
 (format t *a*)
+
+(defvar *b* (list :a 1 :b 2 :c 3))
+(format t "*b*: ~a~%" *b*)
+
+(format t ":a of *b*: ~a~%" (getf *b* :a))
+(setf (getf *b* :a) 4)
+(format t "*b*: ~a~%" *b*)
