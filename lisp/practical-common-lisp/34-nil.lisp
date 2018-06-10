4c540a10631b9faa737ae54e3cb8cd06a7811d8f
Sunaina Pai <sunainapai.in@gmail.com>
2018-06-10 20:48:59 +0530

    Practical Common Lisp: nil, numbers

---
+++ lisp/practical-common-lisp/34-nil.lisp
@@ -0,0 +1,4 @@
+(format t "~a~%" ())
+(format t "~a~%" nil)
+(format t "~a~%" (equal () nil))
+(format t "~a~%" (equal nil ()))
---
+++ lisp/practical-common-lisp/35-numbers.lisp
@@ -0,0 +1,5 @@
+(format t "~a~%" 1) ; 1
+(format t "~a~%" 1/4) ; 1/4
+(format t "~a~%" (equal 1/4 2/8)) ; T
+(format t "~a~%" (equal 1/4 0.25)) ; NIL
+(format t "~a~%" (+ 1/2 1/3)) ; 5/6
