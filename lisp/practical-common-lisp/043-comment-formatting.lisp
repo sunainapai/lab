ce591f716bf1b77c5961a97424496da7d3f41c8e
Sunaina Pai <sunainapai.in@gmail.com>
2018-07-14 16:20:42 +0530

    Practical Common Lisp: Formatting comments

---
+++ lisp/practical-common-lisp/043-comment-formatting.lisp
@@ -0,0 +1,14 @@
+;;;; Four semicolons are used for a file header comment.
+
+;;; A comment with three semicolons will usually be a paragraph
+;;; comment that applies to a large section of code that follows,
+
+(defun foo (x)
+  (dotimes (i x)
+    ;; Two semicolons indicate this comment applies to the code
+    ;; that follows. Note that this comment is indented the same
+    ;; as the code that follows.
+    (format t "foo~%")       ; this comment applies to this line only
+    (format t "bar~%")))     ; and this is for this line
+
+(foo 3)
