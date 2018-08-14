d9849439db10aa55af2d2481f6a1611418cada1d
Sunaina Pai <sunainapai.in@gmail.com>
2018-08-14 20:02:12 +0530

    Practical Common Lisp: Different keyword parameter name in call

---
+++ lisp/practical-common-lisp/050-key-call-param.lisp
@@ -0,0 +1,13 @@
+(defun f (&key a (b a) (c (+ a b)))
+  (format t "a: ~a; b: ~a; c: ~a~%" a b c))
+
+(f :a 10) ; a: 10; b: 10; c: 20
+
+(defun g (&key a ((:box b) a)  ((:cat c) (+ a b) c-supplied-p))
+  (format t "a: ~a; b: ~a; c: ~a; c-supplied-p: ~a~%" a b c c-supplied-p))
+
+; Unknown &KEY argument: :B
+; (g :a 10 :b 20)
+
+(g :a 10 :box 20) ; a: 10; b: 20; c: 30; c-supplied-p: NIL
+(g :a 10 :box 20 :cat 100) ; a: 10; b: 20; c: 100; c-supplied-p: T
