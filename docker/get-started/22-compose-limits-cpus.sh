0baefed594e60608e083aa5c91387ec27ec2dc27
Sunaina Pai <sunainapai.in@gmail.com>
2020-06-06 18:23:19 +0530

    Docker: Get started: Add note on running script

--- docker/get-started/22-compose-limits-cpus.sh
+++ docker/get-started/22-compose-limits-cpus.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# This script should be run in a Linux VM because we want to see the
+# processes running inside a container in the output of the `top`
+# command run on the host. On macOS, the host kernel is not Linux, so
+# the `top` command cannot see processes running in Docker containers
+# that run in a separate Linux kernel packaged with Docker. Only on
+# Linux, Docker would reuse the host Linux kernel to run Docker
+# containers, so all processes running in the container are also running
+# in the host kernel, so `top` command can display them.
+
 set -x
 docker build -t pyloop 22-compose-limits-cpus
 docker swarm init
